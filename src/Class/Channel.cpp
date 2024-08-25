32fc9c7498dcb1f78a97aef8de5f69f68b11fc03
Doboy <dorian.boire98@gmail.com>
2024-08-25 20:28:13 +0200

    Cleaning channel

--- src/Class/Channel.cpp
+++ src/Class/Channel.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dboire <dboire@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 21:07:07 by dboire            #+#    #+#             */
-/*   Updated: 2024/08/25 15:53:46 by aranger          ###   ########.fr       */
+/*   Updated: 2024/08/25 20:27:36 by dboire           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,32 +135,13 @@ Channel& Channel::operator=(const Channel& src)
 {
 	_channel_topic = src._channel_topic;
 	_clients = src._clients;
-	_operators = src._operators;
+	_operator = src._operator;
 	_server = src._server;
 	_key = src._key;
 	_limit_user = src._limit_user;
 	_topic_protection = src._topic_protection;
 	_invite_only = src._invite_only;
 	return (*this);
-}
-
-void	Channel::removeOperatorPrivilege(std::string username)
-{
-	std::map<int,std::string>::iterator it = _operators.begin();
-	std::map<int,std::string>::iterator ite = _operators.end();
-	for(; it != ite; it++)
-	{
-		if (it->second == username)
-		{
-			_operators.erase(it);
-			break;
-		}
-	}
-}
-
-void	Channel::addOperatorPrivilege(std::string username)
-{ 
-	(void)username;
 }
 
 // std::string				Channel::getChannelTopic()
@@ -171,39 +152,6 @@ void	Channel::addOperatorPrivilege(std::string username)
 std::string				Channel::getKey()
 {
 	return (_key);
-}
-
-bool					Channel::isModeOn(char mode)
-{
-	int r_value = 0;
-	switch (mode)
-	{
-	case 'i':
-		r_value = _invite_only;
-		break;
-	case 't':
-		r_value = _topic_protection;
-		break;
-	case 'k':
-		if (_key != "")
-			r_value = 1;
-		break;
-	case 'l':
-		if (_limit_user != 0)
-			r_value = 1;
-		break;		
-	default:
-		break;
-	}
-	return (r_value);
-}
-
-bool					Channel::isOperator(int socket_user)
-{
-	if (_operators.find(socket_user) != _operators.end())
-		return true;
-	else 
-		return false;
 }
 
 void	Channel::addClientToInvite(Client * client, Client * t_client)
@@ -218,48 +166,9 @@ void	Channel::addClientToInvite(Client * client, Client * t_client)
 	sendMessageToClient(client->getSocket(), MSG_INVITER(client->getNick(), t_client->getNick(), this->getName()));
 }
 
-void	Channel::setUnsetInviteMode(bool on_off)
-{
-	_invite_only = on_off;
-}
-
-void	Channel::setUnsetTopicRestr(bool on_off)
-{
-	_topic_protection = on_off;
-}
-
-void	Channel::setUnsetPassword(bool on_off, std::string password)
-{
-	if (on_off)
-		_key = password;
-	else
-		_key = "";
-}
-
-void	Channel::setUnsetUserLimit(bool on_off, size_t user_limit)
-{
-	if (on_off)
-		_limit_user = user_limit;
-	else
-		_limit_user = 0;
-}
-
-void	Channel::setUnsetOpPrivilege(bool on_off, std::string username)
-{
-	if (on_off)
-		addOperatorPrivilege(username);
-	else
-		removeOperatorPrivilege(username);
-}
-
 size_t	Channel::getUserLimit()
 {
 	return (_limit_user);
-}
-
-void	Channel::setName(std::string name)
-{
-	this->_name = name;
 }
 
 std::string Channel::getName()
--- src/Class/Channel.hpp
+++ src/Class/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dboire <dboire@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 21:07:14 by dboire            #+#    #+#             */
-/*   Updated: 2024/08/25 14:28:30 by aranger          ###   ########.fr       */
+/*   Updated: 2024/08/25 20:28:02 by dboire           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,52 +31,38 @@ class Channel
 		void					addClientToCh(Client * client);
 		void					addClientToOp(Client * client);
 		void					addClientToInvite(Client * client, Client * t_client);
-		void					delClientToOp(Client* client);
-		
+
 		void					kickClient(Client* client, std::string target, std::string reason);
+		void					delClientToOp(Client* client);
 		void					delClient(std::string client);
 		void					delChannel();
-		
+
 		bool					checkLimitUser();
 		bool					checkInvite(std::string name);
-		
 		bool					checkIfOp(std::string name);
 		
 		void					notifyJoin(std::string nickname);
-		
 		void					sendMessageToAllClient(std::string error);
 		void					sendMessageToAllClient(std::string sender, std::string message);
 
-		size_t						getUserLimit();
-		std::string 				getName();
-		std::string					getKey();
-		size_t						getLimitUser();
-		bool						getInvite();
-		bool						getTopicProtection();
-		std::string 				getTopic();
+		size_t					getUserLimit();
+		std::string 			getName();
+		std::string				getKey();
+		size_t					getLimitUser();
+		bool					getInvite();
+		bool					getTopicProtection();
+		std::string 			getTopic();
 		
-		void						setKey(std::string key);
-		void						setTopicMsg(std::string topic);
-		void						setLimit(std::string limit);
-		void						setTopic(int remove);
-		void						setInvite(int remove);
+		void					setKey(std::string key);
+		void					setTopicMsg(std::string topic);
+		void					setLimit(std::string limit);
+		void					setTopic(int remove);
+		void					setInvite(int remove);
 
-		bool					isModeOn(char mode);
-		bool					isOperator(int socket_user);
-		
 		bool					hasUser(std::string nickname);
-
-		void 					setUnsetTopicRestr(bool on_off);
-		void 					setUnsetInviteMode(bool on_off);
-		void 					setUnsetPassword(bool on_off, std::string password);
-		void 					setUnsetUserLimit(bool on_off, size_t user_limit);
-		void					setUnsetOpPrivilege(bool on_off, std::string username);
-		void					setName(std::string name);
 		void					printUsersInChannel(Client* client, std::string& channel_name);
 
 	private :	
-		void					removeOperatorPrivilege(std::string username);
-		void					addOperatorPrivilege(std::string username);
 		
 		std::map<std::string, Client *>	_clients;
 		std::map<std::string, Client *>	_operator; // +o
@@ -88,11 +74,6 @@ class Channel
 		bool							_invite_only; // +i
 		bool							_topic_protection; // +t
 		std::string						_channel_topic;
-		
-		
-		
-		// To delete
-		std::map<int, std::string>	_operators;
 };
 
 #endif
