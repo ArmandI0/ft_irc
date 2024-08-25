b9d2bc66ec204c0612bd376560a5ebf2077e4627
Doboy <dorian.boire98@gmail.com>
2024-08-25 13:27:00 +0200

    Size fix

--- src/Class/Command.cpp
+++ src/Class/Command.cpp
@@ -212,7 +212,7 @@ void	Command::execTopic(std::vector<std::string> & command)
 
 void	Command::execMode(std::vector<std::string> & command)
 {
-	if(command.size() < 2)
+	if(command.size() <= 2 || command.size() >= 5)
 		sendMessageToClient(this->_client_requester->getSocket(), ERR_NEEDMOREPARAMS(this->_client_requester->getNick(), command[0]));
 	else
 	{
@@ -335,7 +335,7 @@ void	Command::execInvite(std::vector<std::string> & command)
 void	Command::execJoin(std::vector<std::string> & command)
 {
 	std::cout << command[1] << std::endl;
-	if(command.size() < 2)
+	if(command.size() < 2 || command.size() > 3)
 		sendMessageToClient(this->_client_requester->getSocket(), ERR_NEEDMOREPARAMS(this->_client_requester->getNick(), command[0]));
 	else if(!command[1].empty())
 	{
@@ -372,7 +372,7 @@ void	Command::execJoin(std::vector<std::string> & command)
 
 void	Command::execKick(std::vector<std::string> & command)
 {
-	if((command[1].empty() || command[2].empty()) || command.size() > 4)
+	if(command.size() != 3)
 		sendMessageToClient(this->_client_requester->getSocket(), ERR_NEEDMOREPARAMS(this->_client_requester->getNick(), command[0]));
 	else
 	{
