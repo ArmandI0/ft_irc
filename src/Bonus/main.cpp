b9377e1405e696e6adc5c9bb90570537eee295d5
nledent <nledent@42.fr>
2024-09-02 10:42:26 +0200

    upd bot error management

--- src/Bonus/main.cpp
+++ src/Bonus/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nledent <nledent@42angouleme.fr>           +#+  +:+       +#+        */
+/*   By: nledent <nledent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 18:51:33 by aranger           #+#    #+#             */
-/*   Updated: 2024/08/28 18:41:10 by nledent          ###   ########.fr       */
+/*   Updated: 2024/09/02 10:39:50 by nledent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ int main(int ac, char** av)
 	{
 		std::cerr << "Error : usage : ./bot <server ip adress> <password> <port> <bot nickname>" << std::endl;
 		return (1);	
+	}
+	for (int i=1;i<5;i++)
+	{
+		std::string arg = av[i];
+		if (arg.empty())
+		{
+			std::cerr << "Error : please provide non null parameters" << std::endl;
+			return (1);	
+		}
 	}
 	Bot bot = Bot(av[1], av[2], av[3], av[4]);
 	try 
